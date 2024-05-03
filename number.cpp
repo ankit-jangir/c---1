4a18512e3d26656e7dba73b3a51ad1fcabbf5182
ankit-jangir <ankitjangir5824@gmail.com>
2024-05-03 11:35:08 +0530

    vector example

---
+++ number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> num;
+    cout << "Enter the number of elements: ";
+    cin >> num;
+    for(int num : num)
+}
