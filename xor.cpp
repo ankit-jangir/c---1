6fa5701e6c99d77cc38800a98d8d9a4c60b6a59a
ankit-jangir <ankitjangir5824@gmail.com>
2024-05-03 11:51:36 +0530

    xor example

--- xor.cpp
+++ xor.cpp
@@ -1,1 +1,22 @@
-  
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+int findUniqueElement(vector<int> value){
+   int uniqueaValue = 0;
+   for(int i=0;i<value.size();i++){
+      uniqueaValue=uniqueaValue ^ value[i];
+
+   }
+   return uniqueaValue;
+}
+int main(){
+   vector<int>value={1,1,5,4,4};
+   int uniqueaValue=findUniqueElement(value);
+   cout<<"Unique Value :"<<uniqueaValue<<endl;
+
+
+
+}
